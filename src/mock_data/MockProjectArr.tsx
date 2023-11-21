8b4b0a33d82f17558f540cf530e6619ad73ef401
Zaozerskikh <zubby749@gmail.com>
2023-11-21 04:32:20 +0300

    minor fixes

--- src/mock_data/MockProjectArr.tsx
+++ src/mock_data/MockProjectArr.tsx
@@ -19,13 +19,13 @@ const puzzles2 = 'https://i.imgur.com/pt8keLO.png'
 
 export const MockProjectArr: ProjectInfo[] = [
   {
-    id: '3',
-    name: 'Audio Guide Paris',
+    id: 'audio-guide-paris',
+    name: 'audio guide paris',
     projectType: ProjectType.MOBILE_APP,
     detailedSharedImages: [audioGuide1, audioGuide2],
     previewImage: audioGuidePreview,
-    shortDescriptionRUS: 'разработал мобильное приложение- путеводитель по Парижу, подключил карты, плеер, оплату, провел интеграцию с CMS',
-    shortDescriptionENG: 'developed a mobile application for a guide to Paris, connected maps, player, payment, and integrated with CMS.',
+    shortDescriptionRUS: 'разработал мобильное приложение- путеводитель по Парижу, подключил карты, плеер, провел интеграцию с CMS',
+    shortDescriptionENG: 'developed a mobile application for a guide to Paris, connected maps, player, and integrated with CMS',
     fullDescriptionRUS: 'Кроссплатформенное мультиязычное приложение - путеводитель по Парижу. В рамках проекта был пройден полный цикл разработки и внедрения программного продукта: от согласования дизайна и написания кода до интеграции с CMS-системой Contentful и публикации готового приложения в магазины Google Play и App Store.',
     fullDescriptionENG: 'Cross-platform multilingual application - a guide to Paris. The project went through a full cycle development and implementation of a software product: from design approval and coding to integration with the Contentful CMS system and publishing the finished application in the Google Play and App Store.',
     tags: [TagType.REACT_NATIVE, TagType.CMS, TagType.GIT],
@@ -33,12 +33,12 @@ export const MockProjectArr: ProjectInfo[] = [
     appStoreLink: 'https://apps.apple.com/us/app/audio-guide-paris/id6471984315'
   },
   {
-    id: '1',
+    id: 'hookah-pt',
     name: 'hookah.pt',
     projectType: ProjectType.WEBSITE,
     detailedSharedImages: [hookah1, hookah2, hookah3, hookah4],
     previewImage: hookahPreview,
-    shortDescriptionRUS: 'разработал сайт для первого кальянного магазина в Португалии, подготовил фронтенд и интеграцию с CMS.',
+    shortDescriptionRUS: 'разработал сайт для первого кальянного магазина в Португалии, подготовил фронтенд и интеграцию с CMS',
     shortDescriptionENG: 'developed the website for the first hookah store in Portugal, created the frontend and CMS integration',
     fullDescriptionRUS: 'Интернет-магазин кальянной продукции в Португалии. В рамках разработки был создан сайт на React, а так же произведена интеграция с CMS Contentful. Сейчас к проекту активно пишется бэкенд, но оценить интерфейс можно уже сейчас по ссылке ниже.',
     fullDescriptionENG: 'Online store of hookah products in Portugal. As a part of the development there was created a website on React. Integration with CMS Contentful was done to manage content for the client. Now this project is in the backend-development stage, but you can take a look on interface by click on the link below.',
@@ -46,15 +46,15 @@ export const MockProjectArr: ProjectInfo[] = [
     websiteLink: 'https://zaozerskikh.github.io/hookah/'
   },
   {
-    id: '2',
-    name: 'Chess Puzzles',
+    id: 'chess-puzzles',
+    name: 'chess puzzles',
     projectType: ProjectType.MOBILE_APP,
     detailedSharedImages: [puzzles1, puzzles2],
     previewImage: puzzlesPreview,
     shortDescriptionRUS: 'создал мобильное приложение для обучения шахматным стратегиям',
     shortDescriptionENG: 'created a mobile application for teaching chess strategies',
-    fullDescriptionRUS: 'Мобильное приложение - cборник задач по шахматам. Все задачи разбиты по темам, присутствует возможность получения подсказок при решении. Проект некоммерческий, был реaлизован из-за личного интереса к шахматам, а так же желания разобраться в тонкостях реализации этой игры на фреймворке React Native.',
-    fullDescriptionENG: 'Mobile application - a collection of chess problems. All problems are divided into topics, and there is an opportunity to receive hints when solving them. The project is non-profit, it was written due to personal interest in chess, as well as the desire to understand how to implement the game of chess using the React Native framework.',
+    fullDescriptionRUS: 'Мобильное приложение - cборник задач по шахматам. Все задачи разбиты по темам, присутствует возможность получения подсказок при решении. Проект был реaлизован из-за личного интереса к шахматам, а также желания разобраться в тонкостях реализации этой игры на фреймворке React Native.',
+    fullDescriptionENG: 'Mobile application - a collection of chess problems. All problems are divided into topics, and there is an opportunity to receive hints when solving them. The project was written due to personal interest in chess, as well as the desire to understand how to implement the game of chess using the React Native framework.',
     tags: [TagType.REACT_NATIVE, TagType.GIT],
   },
 ]
